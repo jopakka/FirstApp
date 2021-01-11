66b640a045159741b1ef5489d82e7e5b4d3b5b7e
jopakka <jopakkamies@gmail.com>
2021-01-11 14:59:57 +0200

    working app with components

--- App.js
+++ App.js
@@ -1,69 +1,14 @@
 import React from 'react';
-import {
-  StyleSheet,
-  Text,
-  View,
-  FlatList,
-  TouchableOpacity,
-  Image,
-  SafeAreaView,
-} from 'react-native';
+import {SafeAreaView} from 'react-native';
 import GlobalStyles from './styles/GlobalStyles';
-import FlatListStyles from './styles/FlatListStyles';
-
-const mediaArray = [
-  {
-    key: '0',
-    title: 'Title 1',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sodales enim eget leo condimentum vulputate. Sed lacinia consectetur fermentum. Vestibulum lobortis purus id nisi mattis posuere. Praesent sagittis justo quis nibh ullamcorper, eget elementum lorem consectetur. Pellentesque eu consequat justo, eu sodales eros.',
-    thumbnails: {
-      w160: 'http://placekitten.com/160/161',
-    },
-    filename: 'http://placekitten.com/2048/1920',
-  },
-  {
-    key: '1',
-    title: 'Title 2',
-    description:
-      'Donec dignissim tincidunt nisl, non scelerisque massa pharetra ut. Sed vel velit ante. Aenean quis viverra magna. Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. Vestibulum tincidunt sapien eu ipsum tincidunt pulvinar. ',
-    thumbnails: {
-      w160: 'http://placekitten.com/160/164',
-    },
-    filename: 'http://placekitten.com/2041/1922',
-  },
-  {
-    key: '2',
-    title: 'Title 3',
-    description:
-      'Phasellus imperdiet nunc tincidunt molestie vestibulum. Donec dictum suscipit nibh. Sed vel velit ante. Aenean quis viverra magna. Praesent eget cursus urna. Ut rhoncus interdum dolor non tincidunt. Sed vehicula consequat facilisis. Pellentesque pulvinar sem nisl, ac vestibulum erat rhoncus id. ',
-    thumbnails: {
-      w160: 'http://placekitten.com/160/167',
-    },
-    filename: 'http://placekitten.com/2039/1920',
-  },
-];
+import List from './components/List';
+import {StatusBar} from 'expo-status-bar';
 
 const App = () => {
   return (
     <SafeAreaView style={GlobalStyles.droidSafeArea}>
-      <FlatList
-        data={mediaArray}
-        renderItem={({item}) => {
-          return (
-            <TouchableOpacity style={FlatListStyles.item}>
-              <Image
-                style={FlatListStyles.image}
-                source={{uri: item.thumbnails.w160}}
-              />
-              <View style={FlatListStyles.textBox}>
-                <Text style={FlatListStyles.title}>{item.title}</Text>
-                <Text style={FlatListStyles.text}>{item.description}</Text>
-              </View>
-            </TouchableOpacity>
-          );
-        }}
-      />
+      <List />
+      <StatusBar style="auto" />
     </SafeAreaView>
   );
 };
---
+++ components/ListItem.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {TouchableOpacity, Image, View, Text} from 'react-native';
+import FlatListStyles from '../styles/FlatListStyles';
+import PropTypes from 'prop-types';
+
+const ListItem = ({singleMedia}) => {
+  return (
+    <TouchableOpacity style={FlatListStyles.item}>
+      <Image
+        style={FlatListStyles.image}
+        source={{uri: singleMedia.thumbnails.w160}}
+      />
+      <View style={FlatListStyles.textBox}>
+        <Text style={FlatListStyles.title}>{singleMedia.title}</Text>
+        <Text style={FlatListStyles.text}>{singleMedia.description}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+ListItem.PropTypes = {
+  singleMedia: PropTypes.object.isRequired,
+};
+
+export default ListItem;
