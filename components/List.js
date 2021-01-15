1a79a2394d0d9c757c25d069e4a3bf4452e19a21
jopakka <jopakkamies@gmail.com>
2021-01-15 09:44:41 +0200

    small changes

--- components/List.js
+++ components/List.js
@@ -2,15 +2,17 @@ import React, {useState, useEffect} from 'react';
 import {FlatList, StyleSheet} from 'react-native';
 import ListItem from './ListItem';
 
+const url =
+  'https://raw.githubusercontent.com/mattpe/wbma/master/docs/assets/test.json';
+
 const List = () => {
   const [mediaArray, setMediaArray] = useState([]);
 
-  const url =
-    'https://raw.githubusercontent.com/mattpe/wbma/master/docs/assets/test.json';
   const loadMedia = async () => {
     try {
       const response = await fetch(url);
       const json = await response.json();
+      console.log(json);
       setMediaArray(json);
     } catch (e) {
       console.error('List loadMedia', e.message);
